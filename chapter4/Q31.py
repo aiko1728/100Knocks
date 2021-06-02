8a93449565f77f1d949dd111416a794e43e29393
nakatani yuki <82087483+aiko1728@users.noreply.github.com>
2021-06-02 13:28:59 +0900

    Update Q31.py

--- chapter4/Q31.py
+++ chapter4/Q31.py
@@ -3,21 +3,21 @@ from Q30 import trans_dict, parse
 def extract(lines):
     surface = []
     for line in lines:
-        _list = list(filter(lambda x: x['pos'] == '動詞', line))
+        word_list = list(filter(lambda x: x['pos'] == '動詞', line))
 
-        for word in _list:
+        for word in word_list:
             surface.append(word['surface'])
 
     return surface
 
+if __name__ == '__main__':
+    args = parse()
+    with open(args.file, 'r') as f:
+        lines = f.read().split('EOS\n')
 
-args = parse()
-with open(args.file, 'r') as f:
-    lines = f.read().split('EOS\n')
+    lines = list(filter(lambda x: x != '', lines))
+    result = [trans_dict(line) for line in lines]
 
-lines = list(filter(lambda x: x != '', lines))
-result = [trans_dict(line) for line in lines]
+    surface = extract(result)
 
-surface = extract(result)
-
-print(surface)
+    print(surface)
