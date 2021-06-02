25544f73fdbd6d095c4cd76a7505cbee8bf97f3d
nakatani yuki <82087483+aiko1728@users.noreply.github.com>
2021-06-02 14:40:58 +0900

    Update Q39.py

--- chapter4/Q39.py
+++ chapter4/Q39.py
@@ -1,33 +1,37 @@
-from Q30 import trans_dict, parse
+from Q30 import trans_dict
+from Q35 import word_counter
 import collections
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import argparse
 #import japanize_matplotlib
 
 sns.set(font='IPAGothic')
 
-def counter(lines):
-    tmp = []
-    for line in lines:
-        for word in line:
-            tmp.append(word['base'])
-    result = collections.Counter(tmp)            
-    
-    return result
+def parse():
+    parser = argparse.ArgumentParser()
 
-args = parse()
-with open(args.file, 'r') as f:
-    lines = f.read().split('EOS\n')
+    parser.add_argument('file')
+    parser.add_argument('--q39', help = 'image file for Q39')
 
-lines = list(filter(lambda x: x != '', lines))
-result = [trans_dict(line) for line in lines]
+    args = parser.parse_args()
 
-num = [x[1] for x in counter(result).most_common()]
+    return args
 
-fig = plt.figure()
+if __name__ == '__main__':
+    args = parse()
+    with open(args.file, 'r') as f:
+        lines = f.read().split('EOS\n')
 
-plt.scatter(np.log(range(1, len(num)+1)), np.log(num))
-plt.show()
+    lines = list(filter(lambda x: x != '', lines))
+    result = [trans_dict(line) for line in lines]
 
-fig.savefig(args.q39)
+    num = [x[1] for x in word_counter(result).most_common()]
+
+    fig = plt.figure()
+
+    plt.scatter(np.log(range(1, len(num)+1)), np.log(num))
+    plt.show()
+
+    fig.savefig(args.q39)
