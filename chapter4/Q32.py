a84461fa6ab690791905b91bece6209d0228a08b
nakatani yuki <82087483+aiko1728@users.noreply.github.com>
2021-06-02 13:29:51 +0900

    Update Q32.py

--- chapter4/Q32.py
+++ chapter4/Q32.py
@@ -3,20 +3,21 @@ from Q30 import trans_dict, parse
 def extract(lines):
     base = []
     for line in lines:
-        _list = list(filter(lambda x: x['pos'] == '動詞', line))
+        word_list = list(filter(lambda x: x['pos'] == '動詞', line))
 
-        for word in _list:
+        for word in word_list:
             base.append(word['base'])
 
     return base
 
-args = parse()
-with open(args.file, 'r') as f:
-    lines = f.read().split('EOS\n')
+if __name__ == '__main__':
+    args = parse()
+    with open(args.file, 'r') as f:
+        lines = f.read().split('EOS\n')
 
-lines = list(filter(lambda x: x != '', lines))
-result = [trans_dict(line) for line in lines]
+    lines = list(filter(lambda x: x != '', lines))
+    result = [trans_dict(line) for line in lines]
 
-base = extract(result)
+    base = extract(result)
 
-print(base)
+    print(base)
